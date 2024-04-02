178af688f79d241373f985800c7ad5e64cfd6f11
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-04-02 18:35:07 +0200

    Forge Build set up

--- src/MiniPrimes.sol
+++ src/MiniPrimes.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.22;
 
-import {ERC721} from "@openzeppelin/contracts/token/ERC721";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MiniPrimes is ERC721 {
-    constructor() ERC721() {}
-
-    function buy() {}
-
-    function dispute() {}
+    constructor() ERC721("MiniPrimes", "PRIME") {}
+    // function buy() {}
+    // function dispute() {}
 }
--- test/MiniPrimes.t.sol
+++ test/MiniPrimes.t.sol
@@ -3,6 +3,6 @@ pragma solidity 0.8.22;
 
 import {Test, console} from "forge-std/Test.sol";
 
-contract MiniPrimes is Test {
+contract MiniPrimesTest is Test {
     function setUp() public {}
 }

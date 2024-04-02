06db1be5854ffd9f31772a454467075a60657894
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-04-02 18:14:39 +0200

    NFT set up

---
+++ src/MiniPrimes.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.22;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721";
+
+contract MiniPrimes is ERC721 {
+    constructor() ERC721() {}
+
+    function buy() {}
+
+    function dispute() {}
+}
---
+++ test/MiniPrimes.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.22;
+
+import {Test, console} from "forge-std/Test.sol";
+
+contract MiniPrimes is Test {
+    function setUp() public {}
+}

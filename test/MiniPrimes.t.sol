0b90f73039f29ed1ddeaf5fa317d37a31106f00c
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-04-08 14:50:15 +0200

    Add Addresses and Setup Test

--- test/MiniPrimes.t.sol
+++ test/MiniPrimes.t.sol
@@ -1,8 +1,33 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.22;
 
-import {Test, console} from "forge-std/Test.sol";
+import "forge-std/Test.sol";
+
+import {MiniPrimes} from "../src/MiniPrimes.sol";
 
 contract MiniPrimesTest is Test {
-    function setUp() public {}
+    // Variables
+    MiniPrimes mini;
+
+    // Addresses
+    address OWNER = makeAddr("Owner");
+    address MINTER = makeAddr("Minter");
+
+    function testSeeAddress() public view {
+        console.log("Addresses are", MINTER, OWNER);
+    }
+
+    function setUp() public {
+        vm.prank(OWNER);
+        mini = new MiniPrimes();
+
+        // give MINTER 100 ETH
+        deal(MINTER, 1e18);
+    }
+
+    function testBuyID() public view {}
+
+    function testPayment() public view {}
+
+    function testSuccessfulBuy() public view {}
 }

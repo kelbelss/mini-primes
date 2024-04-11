c2120c478eed56fd1345fcce97529ed8e06abbcd
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-04-11 14:39:34 +0200

    Test Progress

--- test/MiniPrimes.t.sol
+++ test/MiniPrimes.t.sol
@@ -6,6 +6,8 @@ import "forge-std/Test.sol";
 import {MiniPrimes} from "../src/MiniPrimes.sol";
 
 contract MiniPrimesTest is Test {
+    uint256 public constant NFT_PRICE = 1 ether; //1e18
+
     // Variables
     MiniPrimes mini;
 
@@ -22,10 +24,28 @@ contract MiniPrimesTest is Test {
         mini = new MiniPrimes();
 
         // give MINTER 100 ETH
-        deal(MINTER, 1e18);
+        deal(MINTER, 100e18);
     }
 
-    function test_buy_fail_tokenIdAlreadyMinted() public view {}
+    function test_buy_fail_tokenIdAlreadyMinted() public {
+        // check balance to ensure checks happen and state is as it should be
+        assertEq(mini.balanceOf(MINTER), 0);
+
+        // Mint a token
+        vm.startPrank(MINTER);
+        mini.buy{value: NFT_PRICE}(MINTER, 1);
+
+        assertEq(mini.balanceOf(MINTER), 1);
+
+        // Try mint same Id
+        vm.expectRevert(MiniPrimes.AlreadyMinted.selector);
+        mini.buy{value: NFT_PRICE}(MINTER, 1);
+
+        assertEq(mini.balanceOf(MINTER), 1);
+
+        // Stop MINTER prank
+        vm.stopPrank();
+    }
 
     function test_buy_fail_InsufficientFunds() public view {}
 

133fee07f4eb4a695d4af4762967bbfecc7b5c1c
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-04-16 18:02:56 +0200

    Deploy Script

--- script/DeployMiniPrimes.s.sol
+++ script/DeployMiniPrimes.s.sol
@@ -1,13 +1,26 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.22;
 
-import {Script, console} from "forge-std/Script.sol";
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
 import {MiniPrimes} from "../src/MiniPrimes.sol";
 
-contract MiniPrimesScript is Script {
+contract DeployMiniPrimesScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        uint256 privateKey = vm.envUint("DEV_PRIVATE_KEY");
+        address account = vm.addr(privateKey);
+
+        console.log("Account", account);
+
+        vm.startBroadcast(privateKey);
+        // DEPLOY TOKEN
+        MiniPrimes mini = new MiniPrimes();
+        // MINT
+
+        vm.stopBroadcast();
+
+        console.log("Contract address: ", address(mini));
     }
 }
